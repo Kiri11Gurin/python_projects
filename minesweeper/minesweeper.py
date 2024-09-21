0404be65a4e5c205d068252e0dbcf46547b77cf0
Kiri11Gurin <164596083+Kiri11Gurin@users.noreply.github.com>
2024-09-21 14:15:24 +0700

    Update minesweeper.py

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,12 +1,12 @@
 import random
 class GamePole:
-    obj = None
+    __instance = None
 
     def __new__(cls, *args, **kwargs):
         """Разрешается создавать только один объект класса GamePole (паттерн Singleton)."""
-        if cls.obj is None:
-            cls.obj = super().__new__(cls)
-        return cls.obj
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self, N, M, total_mines):
         self.N = N
