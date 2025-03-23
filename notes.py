505cdd5a6de513a4f4c31ae29c14114383c7b534
Kiri11Gurin <164596083+Kiri11Gurin@users.noreply.github.com>
2025-03-23 11:18:31 +0700

    Add files via upload

--- notes.py
+++ notes.py
@@ -5209,20 +5209,55 @@ def f(x):
     return x**2
 
 
+def h(x):
+    return np.sin(x)
+
+
+def k(x):
+    return np.cos(x)
+
+
+def m(x):
+    return np.tan(x)
+
+
+# 1-ый график
 x = np.arange(0, 5, 0.1)
 fig = px.scatter(x=x, y=f(x))  # построение точечного графика
 fig.show()
 
+# 2-ой график
 fig = go.Figure()
-fig.update_xaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='#008000')
-fig.update_yaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='LightPink')
+fig.update_xaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='#008000')  # осевая линия по "x"
+fig.update_yaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='LightPink')  # осевая линия по "y"
 fig.add_trace(go.Scatter(x=x, y=f(x), mode='lines+markers',  name='f(x)=x<sup>2</sup>'))
 fig.add_trace(go.Scatter(x=x, y=x, mode='markers', name='g(x)=x',
                          marker=dict(color='LightSkyBlue', size=10, line=dict(color='Red', width=3))))
+fig.update_layout(legend_orientation="h",  # положение легенды по вертикали
+                  legend=dict(x=.5, xanchor="center"),  # положение легенды по горизонтали
+                  hovermode="x",  # показывает значение "x" и "y" сразу у обоих графиков
+                  margin=dict(l=0, r=0, t=0, b=0))  # поля графика
+fig.update_traces(hoverinfo="all", hovertemplate="Аргумент: %{x}<br>Функция: %{y}")  # подписи при наведении мышки
+fig.show()
+
+# 3-ий график
+fig = make_subplots(rows=2, cols=2,
+                    specs=[[{"rowspan": 2}, {}],  # матрица 2х2 с описанием размера графиков
+                           [None, {}]])  # None означает, что графика с координатами (1, 0) не существует
+fig.update_yaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='LightPink', col=2)
+fig.update_xaxes(range=[-0.5, 1.5], zeroline=True, zerolinewidth=2, zerolinecolor='#008000', col=2)
+fig.add_trace(go.Scatter(x=x, y=h(x),  name='h(x)=sin(x)'), 2, 2)
+fig.add_trace(go.Scatter(x=x, y=k(x),  name='k(x)=cos(x)'), 2, 2)
+fig.add_trace(go.Scatter(x=x, y=m(x),  name='m(x)=tg(x)'), 1, 1)
+fig.add_trace(go.Scatter(x=x, y=f(x), mode='lines+markers',  name='f(x)=x<sup>2</sup>'), 1, 2)
+fig.add_trace(go.Scatter(x=x, y=x, mode='markers', name='g(x)=x',
+                         marker=dict(color='LightSkyBlue', size=20, line=dict(color='MediumPurple', width=3))), 1, 2)
 fig.update_layout(legend_orientation="h",
                   legend=dict(x=.5, xanchor="center"),
-                  hovermode="x",  # показывает значение "x" и "y" сразу у обоих графиков
-                  margin=dict(l=0, r=0, t=0, b=0))
+                  hovermode="x",
+                  margin=dict(l=0, r=0, t=0, b=0),
+                  height=650,
+                  width=1500)
 fig.update_traces(hoverinfo="all", hovertemplate="Аргумент: %{x}<br>Функция: %{y}")
 fig.show()
 '''
